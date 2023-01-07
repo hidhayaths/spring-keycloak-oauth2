5fac82dd9c01206fef5b98efc15a95919fdb2e1b
Hidhayath <hidhayaths@outlook.com>
2023-01-07 22:07:11 +0530

    removed security config & jwt converter as keycloak scopes are configured and handled to tackle roles and permissions

--- src/main/java/com/hidhayaths/springkeycloakoauth2/controller/HomeController.java
+++ src/main/java/com/hidhayaths/springkeycloakoauth2/controller/HomeController.java
@@ -1,6 +1,6 @@
 package com.hidhayaths.springkeycloakoauth2.controller;
 
-import jakarta.annotation.security.RolesAllowed;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +17,7 @@ public class HomeController {
         return "hello, "+principal.getName();
     }
 
-    @RolesAllowed({"ADMIN"})
+    @PreAuthorize("hasAuthority('SCOPE_write')")
     @GetMapping("/admin")
     public String greetAdmin(Principal principal){
         return "hello admin, "+principal.getName();
